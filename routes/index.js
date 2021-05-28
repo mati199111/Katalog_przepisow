c62ba8a964c59edc67f3f7d769df9652bb50382f
mati199111 <mati199111@gmail.com>
2021-05-28 17:51:27 +0200

    28052021

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,49 @@
 var express = require('express');
 var router = express.Router();
 
+const mongoose = require('mongoose');
+const a = require('../models/recipe-api')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Recipe' });
+    res.render('index', {
+        title: 'Recipe',
+        isIndex: true
+    });
 });
 
+
+router.get('/query', (req, res, next) => {
+    a.Recipe.find({
+        $or: [
+            { 'type': { $regex: req.query.search, $options: 'i' } },
+            { 'title': { $regex: req.query.search, $options: 'i' } }
+        ]
+    }, (err, recipe) => {
+        if (err) {
+            console.log(err)
+        } else {
+            if (!recipe.length) {
+                res.render('index', {
+                    title: 'Recipe',
+                    isQuery: false,
+                    message: 'Query returned zero'
+                })
+            } else if (req.query.search === '' || req.query.search === '') {
+                res.render('index', {
+                    title: 'Recipe',
+                    isQuery: false,
+                    message: 'Cannot query empty string'
+                })
+            } else {
+                res.render('index', {
+                    title: 'Recipe',
+                    isQuery: true,
+                    recipe: recipe
+                })
+            }
+        }
+    })
+});
 
 module.exports = router;
